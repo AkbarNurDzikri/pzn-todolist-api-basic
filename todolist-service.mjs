13574f08c68b18cb85ddac93ce65a8ff1a688214
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2023-07-10 13:54:21 +0700

    first commit

---
+++ todolist-service.mjs
@@ -0,0 +1,43 @@
+export class TodoListService {
+  todoList = ['Programmer', 'Zaman', 'Now'];
+
+  getJsonTodoList() {
+    return JSON.stringify({
+      code: 200,
+      status: 'OK',
+      data: this.todoList.map((val, index) => {
+        return {
+          id: index,
+          todo: val,
+        }
+      })
+    })
+  }
+
+  getTodoList(req, res) {
+    res.write(this.getJsonTodoList());
+    res.end();
+  }
+
+  createTodo(req, res) {
+    req.addListener('data', (data) => {
+      const body = JSON.parse(data.toString());
+      this.todoList.push(body.todo);
+
+      res.write(this.getJsonTodoList());
+      res.end();
+    })
+  }
+
+  updateTodo(req, res) {
+    req.addListener('data', (data) => {
+      const body = JSON.parse(data.toString());
+      if(this.todoList[body.id]) {
+        this.todoList[body.id] = body.todo;
+      }
+
+      res.write(this.getJsonTodoList());
+      res.end();
+    })
+  }
+}
